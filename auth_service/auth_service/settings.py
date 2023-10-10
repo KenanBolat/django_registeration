08522dd1a89b413ee8a96f68748f8004c17047fc
KenanBolat <kenan23@gmail.com>
2023-10-10 17:06:39 +0300

    dockerfile service

--- auth_service/auth_service/settings.py
+++ auth_service/auth_service/settings.py
@@ -124,8 +124,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-    ),
+
+    'rest_framework.authentication.SessionAuthentication',
+    'rest_framework.authentication.BasicAuthentication',
+    'rest_framework_simplejwt.authentication.JWTAuthentication',
+),
 }
 
 import datetime
